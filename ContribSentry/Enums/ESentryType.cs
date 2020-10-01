8b1ed10adbf08d34d638052f88c81792e000019e
LucasZF <lucas-zimerman1@hotmail.com>
2020-10-01 11:07:31 -0300

    Hot/wrong enum values in mono (#14)
    Mono Json serialization ignores enums if they don't provide an EnumMember value.
    this hotfix fixes the issue where Sessions lacked the State enum, not giving the correct session status nor session duration.

--- ContribSentry/Enums/ESentryType.cs
+++ ContribSentry/Enums/ESentryType.cs
@@ -1,12 +1,25 @@
-﻿namespace ContribSentry.Enums
+﻿using System.Runtime.Serialization;
+
+namespace ContribSentry.Enums
 {
     public enum ESentryType
     {
+        [EnumMember(Value = "session")]
         Session,
+
+        [EnumMember(Value = "event")]
         Event,
+
+        [EnumMember(Value = "attachment")]
         Attachment,
+
+        [EnumMember(Value = "attachment")]
         Transaction,
+
+        [EnumMember(Value = "unknown")]
         Unknown,
+
+        [EnumMember(Value = "currentSession")]
         CurrentSession
     }
 }
